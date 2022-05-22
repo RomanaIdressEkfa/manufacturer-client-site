24e31b5f15513c0fed8aaf67a5bad43289d20e17
RomanaIdressEkfa <ekfaeshita@gmail.com>
2022-05-23 03:44:56 +0600

    adding image

--- src/Pages/Home/CardGlass.js
+++ src/Pages/Home/CardGlass.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import logo from '../../assets/images/7.jpg'
+import logo from '../../assets/images/5.jpg';
+
 
 const CardGlass = () => {
     return (
-        <div class="card w-96 glass h-screen">
-            <figure><img src={logo} alt="car!" /></figure>
-            <div class="card-body">
-                <h2 class="card-title">Life hack</h2>
-                <p>How to park your car at your garage?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Learn now!</button>
-                </div>
+        <section style={{
+            background: `url(${logo})`
+        }} className='flex justify-center items-center mt-20 m-4 p-12 '>
+            <div className='flex-1 hidden lg:block'>
+                {/* <img className='mt-[-150px]' src={logo2} alt="" /> */}
             </div>
-        </div >
+            <div className='flex-1'>
+                <p className='text-primary font-bold'>Appointment</p>
+                <h1 className='text-4xl font-bold text-white'>Make an appointment Today</h1>
+                <p className='text-white'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                <button className="btn btn-primary font-bold uppercase bg-gradient-to-r from-secondary to-primary text-white">Get Started</button>
+            </div>
+
+        </section>
     );
 };
 
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Children, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import UseAdmin from '../Hooks/UseAdmin';
+import logo from '../../assets/images/hamber.jpg'
 
 const Navbar = ({ children }) => {
     const [dark, setDark] = useState(false)
@@ -25,7 +26,7 @@ const Navbar = ({ children }) => {
                     <label for="my-drawer-2" class="btn btn-square btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                     </label>
-                    <div class="flex-1 px-2 mx-2 text-2xl">Manufacturer</div>
+                    <div class="flex-1 px-2 mx-2 text-2xl"><img className='w-24 h-22' src={logo} alt="" /> </div>
                     <div class="flex-none lg:hidden">
                         <label for="my-drawer-3" class="btn btn-square btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
