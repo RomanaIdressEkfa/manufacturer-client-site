da6a73bc00d3b4eeb4714b49154678b7eb696d16
RomanaIdressEkfa <ekfaeshita@gmail.com>
2022-05-23 20:17:52 +0600

    i am adding svg icon

--- src/Pages/Home/StatUi.js
+++ src/Pages/Home/StatUi.js
@@ -1,14 +1,41 @@
 import React from 'react';
-// import StatsHorizontal from '@components/widgets/stats/StatsVertical'
+import logo from '../../assets/images/3-1.jpg'
+
 
 const StatUi = () => {
-    // const Component = () => <StatsVertical icon={<Eye size={21} />} color='info' stats='36.9k' statTitle='Views' />
+
     return (
-        <div class=" ">
 
-
+        <div className=' mx-auto px-28 py-36 mb-10 w-full' style={{ backgroundImage: `url(${logo})` }}>
+            <div className='bg-slate-300 grid md:grid-cols-1 lg:grid-cols-4 '>
+                <div className=' mr-10'>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+                    </svg>
+                    <h1 className='text-4xl font-extrabold'>100+ <br />Customers </h1>
+                </div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+                    </svg>
+                    <h1 className='text-4xl font-extrabold'>120M+ <br /> Annual revenue</h1>
+                </div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    <h1 className='text-4xl font-extrabold'>33K+ <br /> Reviews </h1>
+                </div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z" clipRule="evenodd" />
+                    </svg>
+                    <h1 className='text-4xl font-extrabold'>50+ <br /> tools</h1>
+                </div>
+            </div>
 
         </div>
+
     );
 };
 
