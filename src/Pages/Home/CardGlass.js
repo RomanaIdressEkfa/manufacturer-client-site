0d25be0ab034e25cb1fbc2677ef9e00212902d26
RomanaIdressEkfa <ekfaeshita@gmail.com>
2022-05-25 23:41:19 +0600

    section color change

--- src/Pages/Home/CardGlass.js
+++ src/Pages/Home/CardGlass.js
@@ -9,8 +9,8 @@ const CardGlass = () => {
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={logo} class="max-w-2xl rounded-lg shadow-2xl " />
                 <div>
-                    <h1 class="text-5xl font-bold">Process Manufacturing!</h1>
-                    <p class="py-6">A manufacturer is a person or company that produces finished goods from raw materials by using various tools, equipment, and processes, and then sells the goods to consumers.</p>
+                    <h1 class="text-5xl font-bold text-neutral-content">Process Manufacturing!</h1>
+                    <p class="py-6 text-neutral-content">A manufacturer is a person or company that produces finished goods from raw materials by using various tools, equipment, and processes, and then sells the goods to consumers.</p>
                     <PrimaryButton></PrimaryButton>
                 </div>
             </div>
